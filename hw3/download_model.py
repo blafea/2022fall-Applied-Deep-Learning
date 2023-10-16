3a02da4b5a729aae5745573dfbb17a854d481134
blafea <86761168+blafea@users.noreply.github.com>
2023-10-16 13:29:58 +0800

    Update download_model.py

--- hw3/download_model.py
+++ hw3/download_model.py
@@ -1,2 +1,2 @@
 import gdown
-gdown.download_folder("https://drive.google.com/drive/folders/1--2DxEarhLuDN8Q5qRY8zar5QFYLIorn?usp=share_link", quiet=True, output="output_dir")
+gdown.download_folder("url", quiet=True, output="output_dir")
