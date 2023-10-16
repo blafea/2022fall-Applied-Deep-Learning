c03fe3f6f2caf2260ba956b4ea2e85a479a3b053
blafea <86761168+blafea@users.noreply.github.com>
2023-10-16 13:29:09 +0800

    Update download.sh

--- hw1/download.sh
+++ hw1/download.sh
@@ -1,4 +1,4 @@
-wget https://www.dropbox.com/s/8box4uxs66w1h1g/best_intent.pth?dl=1 -O best_intent.pth
-wget https://www.dropbox.com/s/1h6v622og5mjp2d/best_slot.pth?dl=1 -O best_slot.pth
-wget https://www.dropbox.com/s/yza3pltp1xbgyhr/cache.zip?dl=1 -O cache.zip
+wget url -O best_intent.pth
+wget url -O best_slot.pth
+wget url -O cache.zip
 unzip cache.zip
